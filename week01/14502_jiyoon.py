05793c2da230b1354fafb9feaee4eed0f5a0959f
Jiyun_Jung <jammy9087@gmail.com>
2023-03-18 16:21:48 +0900

    [BOJ] 14502 연구소

--- week01/14502_jiyoon.py
+++ week01/14502_jiyoon.py
@@ -1,7 +1,8 @@
+from collections import deque
+import copy
 import sys
-import queue
 input = sys.stdin.readline
-
+ 
 N, M = tuple(map(int, input().strip().split(' ')))
 
 lab = []
@@ -9,38 +10,41 @@ for _ in range(N):
     row = list(map(int, input().strip().split(' ')))
     lab.append(row)
 
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-dir = [(1,0), (-1,0), (0,1), (0,-1)]
 def infect():
-
-    q = queue()
-    visit = set()
-
+    q = deque()
+    test_lab = copy.deepcopy(lab)
     for i in range(N):
         for s in range(M):
-            if lab[i][s] == 2:
-                
-                for d in dir:
-                    x, y = d
-                    if i+y < N and s+x < M and lab[i][s] == 0:
-                        lab[i][s] = 2
-                
+            if test_lab[i][s] == 2:
+                q.append((i,s))
 
-def check():
-    num = 0
+    while q:
+        i,s = q.popleft()
+
+        for d in range(4):
+            ny = i+dy[d]
+            nx = s+dx[d]
+
+            if (0<= ny < N) and (0<= nx < M) and test_lab[ny][nx] == 0:
+                test_lab[ny][nx] = 2
+                q.append((ny, nx))
+
+    global result
+    count = 0
     for i in range(N):
-        for s in range(M):
-            if lab[i][s] == 0:
-                num += 1
+        for k in range(M):
+            if test_lab[i][k] == 0:
+                count +=1
 
-    return num
-
+    result = max(result, count)
+    
 
 def make_wall(count):
     if count == 3:
         infect()
-        global score
-        score = max(score, check())
         return
     for i in range(N):
         for k in range(M):
@@ -49,6 +53,6 @@ def make_wall(count):
                 make_wall(count+1)
                 lab[i][k] = 0
 
-score = 0
+result = 0 
 make_wall(0)
-print(score)
+print(result)
