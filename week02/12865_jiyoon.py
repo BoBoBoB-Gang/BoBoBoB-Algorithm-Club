7e9c04a3da80bc0e8193ed0db75222cd37a39642
Jiyun_Jung <jammy9087@gmail.com>
2023-03-20 19:09:19 +0900

    [BOJ] 1715 카드 정렬하기 (python) wq

---
+++ week02/12865_jiyoon.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().strip().split(' '))
+d = [[0]*(K+1) for _ in range(N+1)]
+item = [[0,0]]
+
+for _ in range(N):
+    item.append(list(map(int, input().strip().split(' '))))
+
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        w = item[i][0]
+        v = item[i][1]
+
+        if j < w:
+            d[i][j] = d[i-1][j]
+        else:
+            d[i][j] = max(d[i-1][j], (d[i-1][j-w]+v))
+
+print(d[N][K])
